7f59a79f3eb9197c96d83535e0b4ca69c8106774
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2023-06-03 13:11:15 +0300

    Finish Part1 & Part2

--- part2/b.sql
+++ part2/b.sql
@@ -1,4 +1,5 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+SELECT name
+FROM Dogs
+ORDER BY name DESC;
--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,7 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+SELECT Users.first_name, Users.last_name
+FROM Users
+  JOIN Favorites ON Users.id = Favorites.user_id
+  JOIN Dogs ON Dogs.id = Favorites.dog_id
+WHERE Dogs.name = 'Zoey';
